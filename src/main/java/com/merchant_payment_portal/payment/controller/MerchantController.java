fe62133129ff526b9022875683d21bbf95a47359
ZawMyintOo1 <myintoozaw000@gmail.com>
2026-06-26 20:53:28 +0630

    Merchant Controller Done

--- src/main/java/com/merchant_payment_portal/payment/controller/MerchantController.java
+++ src/main/java/com/merchant_payment_portal/payment/controller/MerchantController.java
@@ -1,15 +1,34 @@
 package com.merchant_payment_portal.payment.controller;
 
+import com.merchant_payment_portal.payment.entity.Merchant;
 import com.merchant_payment_portal.payment.entity.MerchantSegment;
 import com.merchant_payment_portal.payment.service.MerchantSegmentService;
+import com.merchant_payment_portal.payment.service.MerchantService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/merchants")
 public class MerchantController {
 
-    private MerchantSegmentService service;
+    private MerchantService service;
 
-    public List<MerchantSegment> getAllMerchant(){
-       return service.getAllMerchantSegments();
+    public List<Merchant> getAllMerchant(){
+       return service.getAllMerchants();
     }
+    public Merchant getMerchantById(@PathVariable Long id){
+        return service.getMerchantById(id);
+    }
+
+    public Merchant saveMerchant(Merchant merchant){
+        return service.saveMerchant(merchant);
+    }
+    public void deleteByMerchantId(Long id){
+        service.deleteMerchantById(id);
+    }
+
 }
--- src/main/java/com/merchant_payment_portal/payment/service/MerchantService.java
+++ src/main/java/com/merchant_payment_portal/payment/service/MerchantService.java
@@ -1,9 +1,30 @@
 package com.merchant_payment_portal.payment.service;
 
+import com.merchant_payment_portal.payment.entity.Merchant;
 import com.merchant_payment_portal.payment.repository.MerchantRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 public class MerchantService {
     private MerchantRepository merchantRepository;
+
+    public List<Merchant> getAllMerchants(){
+       return merchantRepository.findAll();
+    }
+
+    public Merchant saveMerchant(Merchant merchant){
+       return merchantRepository.save(merchant);
+    }
+
+    public Merchant getMerchantById(@PathVariable Long id){
+        return merchantRepository.findById(id).orElseThrow();
+    }
+
+    public void deleteMerchantById(@PathVariable Long id){
+        merchantRepository.deleteById(id);
+    }
+
 }
