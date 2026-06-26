6fbe771de5ba0b0b662c2eabb782dbe287d33407
zawmyintoo <zawmyintoopg@gmail.com>
2026-06-26 20:39:31 +0630

    Commit Message

--- src/main/java/com/merchant_payment_portal/payment/controller/MerchantController.java
+++ src/main/java/com/merchant_payment_portal/payment/controller/MerchantController.java
@@ -1,4 +1,15 @@
 package com.merchant_payment_portal.payment.controller;
 
+import com.merchant_payment_portal.payment.entity.MerchantSegment;
+import com.merchant_payment_portal.payment.service.MerchantSegmentService;
+
+import java.util.List;
+
 public class MerchantController {
+
+    private MerchantSegmentService service;
+
+    public List<MerchantSegment> getAllMerchant(){
+       return service.getAllMerchantSegments();
+    }
 }
